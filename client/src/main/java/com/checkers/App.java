5a332230198d5fcb3b8143297e3ef72bf8823535
Grubre <ogrodowczykjakub@gmail.com>
2022-12-22 02:47:05 +0100

    feat: Basic board client-side rendering

--- client/src/main/java/com/checkers/App.java
+++ client/src/main/java/com/checkers/App.java
@@ -7,15 +7,14 @@ import javafx.animation.ScaleTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 
 /**
@@ -28,52 +27,16 @@ public class App extends Application {
     public void start(Stage stage) {
         mStage = stage;
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 500, 500, Color.BLACK);
- 
-        Rectangle r = new Rectangle(25,25,250,250);
-        r.setFill(Color.BLUE);
-        root.getChildren().add(r);
+        Group group = new Group();
+        
+        Scene scene = new Scene(group, 500, 500, Color.WHITE);
 
-        TranslateTransition translate = 
-        new TranslateTransition(Duration.millis(750)); 
-        translate.setToX(390); 
-        translate.setToY(390); 
- 
-        FillTransition fill = new FillTransition(Duration.millis(750)); 
-        fill.setToValue(Color.RED); 
- 
-        RotateTransition rotate = new RotateTransition(Duration.millis(750)); 
-        rotate.setToAngle(360); 
- 
-        ScaleTransition scale = new ScaleTransition(Duration.millis(750)); 
-        scale.setToX(0.1); 
-        scale.setToY(0.1); 
- 
-        ParallelTransition transition = new ParallelTransition(r, 
-        translate, fill, rotate, scale); 
-        transition.setCycleCount(Timeline.INDEFINITE);
-        transition.setAutoReverse(true); 
-        transition.play(); 
+        Grid grid = new Grid(8,8);
 
-        Pane pane = new Pane();
-
-        Scene scene2 = new Scene(pane, 500, 500, Color.WHITE);
-
-        scene.addEventFilter(
-         KeyEvent.KEY_PRESSED,
-         event -> {
-            mStage.setScene(scene2);
-        });
-
-        scene2.addEventFilter(
-         KeyEvent.KEY_PRESSED,
-         event -> {
-            mStage.setScene(scene);
-        });
+        group.getChildren().addAll(grid);
  
         mStage.setTitle("Checkers");
-        mStage.setScene(scene2);
+        mStage.setScene(scene);
         mStage.show();
     }
 
---
+++ client/src/main/java/com/checkers/Grid.java
@@ -0,0 +1,83 @@
+package com.checkers;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * A container which extends GridPane with added support for multithreaded Cells.
+ */
+public class Grid extends GridPane {
+    private final int x_dim;
+    private final int y_dim;
+
+    private Board board;
+
+    Grid(int x_dim, int y_dim)
+    {
+        this.x_dim = x_dim;
+        this.y_dim = y_dim;
+
+        addRowsAndColumns();
+
+        for(int j = 0; j < y_dim; j++)
+        {
+            for(int i = 0; i < x_dim; i++)
+            {
+                Rectangle rect = new Rectangle();
+                if((i + j) % 2 == 0)
+                    rect.setFill(Color.BROWN);
+                else
+                    rect.setFill(Color.WHITE);
+                rect.setWidth(100);
+                rect.setHeight(100);
+                add(rect, i, j);
+            }
+        }
+    }
+
+    public void SetAnchorPaneLayout(Boolean Top, Boolean Left, Boolean Bottom, Boolean Right)
+    {
+        if(Top)    AnchorPane.setTopAnchor(this, 0.0);
+        if(Left)   AnchorPane.setLeftAnchor(this, 0.0);
+        if(Bottom) AnchorPane.setBottomAnchor(this, 0.0);
+        if(Right)  AnchorPane.setRightAnchor(this, 0.0);
+    }
+
+    /**
+     * A method which adds gridWidth amount of columns
+     * and gridHeight amount of rows.
+     */
+    private void addRowsAndColumns()
+    {
+        for (int i = 0; i < x_dim; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setFillWidth(true);
+            colConst.setPercentWidth(100.0 / x_dim);
+            getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < y_dim; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setFillHeight(true);
+            rowConst.setPercentHeight(100.0 / y_dim);
+            getRowConstraints().add(rowConst);
+        }
+    }
+
+    
+    /**
+     * @return the width of the grid.
+     */
+    public int getX_dim(){return x_dim;}
+    /**
+     * @return the height of the grid.
+     */
+    public int getY_dim(){return y_dim;}
+}
