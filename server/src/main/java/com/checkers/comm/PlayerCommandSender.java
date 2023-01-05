df43c36a41bca6a8f98f34f293ed7f83c699d265
Dawiderter <dwalczak456@gmail.com>
2023-01-06 00:58:16 +0100

    Test: Added tests for PlayerCommandSender

--- server/src/main/java/com/checkers/comm/PlayerCommandSender.java
+++ server/src/main/java/com/checkers/comm/PlayerCommandSender.java
@@ -6,6 +6,12 @@ public class PlayerCommandSender extends CommandSender {
     int playerId;
     CommandParser parser;
 
+    public PlayerCommandSender(int playerId, CommandParser parser, CommandListener listener) {
+        this.playerId = playerId;
+        this.parser = parser;
+        this.listener = listener;
+    }
+    
     public void onMessage(String message) {
         Command command = parser.parse(message);
         
---
+++ server/src/test/java/com/checkers/comm/PlayerCommandSenderTest.java
@@ -0,0 +1,52 @@
+package com.checkers.comm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.checkers.comm.command.Command;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PlayerCommandSenderTest {
+    int testId = 100;
+
+    String testText = "TEST PARSE";
+
+    Command testCommand = new Command() {
+        @Override
+        public void accept(CommandVisitor visitor) {
+        }
+    };
+
+    @Mock
+    CommandParser parser;
+
+    @Mock
+    CommandListener listener;
+
+    @Captor
+    ArgumentCaptor<Command> commandCaptor;
+
+    @Test
+    public void correctInjectingPlayerDataInCommands() {
+        PlayerCommandSender p = new PlayerCommandSender(testId, parser, listener);
+
+        Mockito.when(parser.parse(testText)).thenReturn(testCommand);
+
+        p.onMessage(testText);
+
+        Mockito.verify(listener).onCommand(commandCaptor.capture());
+
+        Command returnedCommand = commandCaptor.getValue();
+
+        assertEquals(returnedCommand.getPlayerId(), testId);
+        assertEquals(returnedCommand.getSource(), p);
+
+    }
+}
