e9ac94699b0f99a474595845ff3fffb04a1830de
Dawiderter <dwalczak456@gmail.com>
2023-01-06 16:20:17 +0100

    WIP: Added ResponseListener with Response stub, a model for outgoing messages

---
+++ server/src/main/java/com/checkers/resp/ResponseListener.java
@@ -0,0 +1,7 @@
+package com.checkers.resp;
+
+import com.checkers.resp.response.Response;
+
+public interface ResponseListener {
+    void onResponse(Response response);
+}
---
+++ server/src/main/java/com/checkers/resp/response/Response.java
@@ -0,0 +1,5 @@
+package com.checkers.resp.response;
+
+public abstract class Response {
+    //TODO
+}
