93202530a5ebeca247adc242ac2529968f48e815
Dawiderter <dwalczak456@gmail.com>
2023-01-06 17:09:57 +0100

    WIP: Changed CommandVisitor from abstract class to interface with default methods

--- server/src/main/java/com/checkers/comm/CommandVisitor.java
+++ server/src/main/java/com/checkers/comm/CommandVisitor.java
@@ -7,27 +7,27 @@ import com.checkers.comm.command.MovePieceCommand;
 import com.checkers.comm.command.NewGameCommand;
 import com.checkers.comm.command.ResignCommand;
 
-public abstract class CommandVisitor {
+public interface CommandVisitor {
     
-    protected abstract void onUnimplemented(Command command);
+    void onUnimplemented(Command command);
     
-    public void visitDisconnect(DisconnectCommand command) {
+    default void visitDisconnect(DisconnectCommand command) {
         onUnimplemented(command);
     }
 
-    public void visitJoinGame(JoinGameCommand command) {
+    default void visitJoinGame(JoinGameCommand command) {
         onUnimplemented(command);
     }
 
-    public void visitMovePiece(MovePieceCommand command) {
+    default void visitMovePiece(MovePieceCommand command) {
         onUnimplemented(command);
     }
 
-    public void visitNewGame(NewGameCommand command) {
+    default void visitNewGame(NewGameCommand command) {
         onUnimplemented(command);
     }
 
-    public void visitResign(ResignCommand command) {
+    default void visitResign(ResignCommand command) {
         onUnimplemented(command);
     }
 }
