d895f39c01753c31b31e95980f32d11582221928
Dawiderter <dwalczak456@gmail.com>
2023-01-06 22:37:03 +0100

    WIP: Switched from Collections to Lists in commands and responses

--- server/src/main/java/com/checkers/comm/command/MovePieceCommand.java
+++ server/src/main/java/com/checkers/comm/command/MovePieceCommand.java
@@ -1,22 +1,23 @@
 package com.checkers.comm.command;
 
 import java.util.Collection;
+import java.util.List;
 
 import com.checkers.comm.CommandVisitor;
 
 public class MovePieceCommand extends Command {
     final int pieceId;
-    final Collection<Integer> tileIds;
+    final List<Integer> tileIds;
 
     public int getPieceId() {
         return pieceId;
     }
 
-    public Collection<Integer> getTileIds() {
+    public List<Integer> getTileIds() {
         return tileIds;
     }
 
-    public MovePieceCommand(int pieceId, Collection<Integer> tileId) {
+    public MovePieceCommand(int pieceId, List<Integer> tileId) {
         this.pieceId = pieceId;
         this.tileIds = tileId;
     }
--- server/src/main/java/com/checkers/resp/response/PieceMovedResponse.java
+++ server/src/main/java/com/checkers/resp/response/PieceMovedResponse.java
@@ -1,13 +1,14 @@
 package com.checkers.resp.response;
 
 import java.util.Collection;
+import java.util.List;
 
 public class PieceMovedResponse extends Response {
     final int playerId;
     final int pieceId;
-    final Collection<Integer> tileIds;
+    final List<Integer> tileIds;
 
-    public PieceMovedResponse(int playerId, int pieceId, Collection<Integer> tileIds) {
+    public PieceMovedResponse(int playerId, int pieceId, List<Integer> tileIds) {
         this.playerId = playerId;
         this.pieceId = pieceId;
         this.tileIds = tileIds;
