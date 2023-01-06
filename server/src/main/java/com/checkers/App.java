51ad45bfd62f33f40e2ba85844659dfd17ad7604
Dawiderter <dwalczak456@gmail.com>
2023-01-07 00:12:04 +0100

    MILESTONE: Implemented main server app

--- server/src/main/java/com/checkers/App.java
+++ server/src/main/java/com/checkers/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Server server = new Server(59801);
+        Server server = new Server(58901);
         try {
             server.start();
         }
