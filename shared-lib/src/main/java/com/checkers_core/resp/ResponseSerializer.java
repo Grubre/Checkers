4d45a484ce51ac8f8cef5f207af3166c3192c8ab
Dawiderter <dwalczak456@gmail.com>
2023-01-11 15:04:34 +0100

    WIP: Added ResponseSerializer interface

--- shared-lib/src/main/java/com/checkers_core/resp/ResponseSerializer.java
+++ shared-lib/src/main/java/com/checkers_core/resp/ResponseSerializer.java
@@ -1,61 +1,7 @@
 package com.checkers_core.resp;
 
-import com.checkers_core.resp.response.EndOfGameResponse;
-import com.checkers_core.resp.response.GameConnectionSuccessfulResponse;
-import com.checkers_core.resp.response.GameConnectionUnsuccessfulResponse;
-import com.checkers_core.resp.response.IncorrectMoveResponse;
-import com.checkers_core.resp.response.PieceMovedResponse;
-import com.checkers_core.resp.response.PlayerDisconnectedResponse;
 import com.checkers_core.resp.response.Response;
-import com.checkers_core.resp.response.WrongCommandResponse;
 
-public class ResponseSerializer implements ResponseVisitor<String> {
-    public String serialize(Response response) {
-        return response.accept(this);
-    }
-
-    @Override
-    public String visitEndOfGame(EndOfGameResponse response) {
-        return "EOG " + response.getWinnerId();
-        
-    }
-
-    @Override
-    public String visitGameConnectionSuccessful(GameConnectionSuccessfulResponse response) {
-        return "GCS " + response.getGameId();
-        
-    }
-
-    @Override
-    public String visitGameConnectionUnsuccessful(GameConnectionUnsuccessfulResponse response) {
-        return "GCU";
-        
-    }
-
-    @Override
-    public String visitIncorrectMove(IncorrectMoveResponse response) {
-        return "INCORRMOVE";
-    }
-
-    @Override
-    public String visitPieceMoved(PieceMovedResponse response) {
-        String stringTileIds = "";
-        for(int tileId : response.getTileIds()) {
-            stringTileIds += tileId + " ";
-        }
-
-        return "MOVED " + response.getPlayerId() + " " + response.getPieceId() + " " + stringTileIds;
-    }
-
-    @Override
-    public String visitPlayerDisconnected(PlayerDisconnectedResponse response) {
-        return "DISCONNECT " + response.getPlayerId();
-        
-    }
-
-    @Override
-    public String visitWrongCommand(WrongCommandResponse response) {
-        return "WRONGCOMM";
-        
-    }
+public interface ResponseSerializer {
+    String serialize(Response response);
 }
