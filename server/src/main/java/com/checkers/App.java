308eb45d706862db0129948a1f6e248e954b91de
Dawiderter <dwalczak456@gmail.com>
2023-01-11 12:35:37 +0100

    CLEAN: Cleaning up merge problems

---
+++ server/src/main/java/com/checkers/App.java
@@ -0,0 +1,21 @@
+package com.checkers;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Server server = new Server(58901);
+        try {
+            server.start();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
